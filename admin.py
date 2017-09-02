2e2367080a695880d5afaa16ba990d64b9d9eb02
JacksonLuu <jacksonluu2006@hotmail.com>
2017-09-02 14:40:07 +1000

    defined attributes and constructor

--- admin.py
+++ admin.py
@@ -1,1 +1,11 @@
-
+class admin:
+    """Common class for all admins
+       Attributes: name, username and password
+       given at the time of registration.
+    """
+  
+    #Constructor
+    def __init__(self, name, username, password):
+        self._name = name
+        self._username = username
+        self._password = password
