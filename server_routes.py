17e76b2d1483736bea00ed1d0e3a9dac098dc8a7
JacksonLuu <jacksonluu2006@hotmail.com>
2017-09-04 20:53:53 +1000

    Create server_routes.py

---
+++ server_routes.py
@@ -0,0 +1,33 @@
+# Survey flask server file
+from flask import Flask, flash, redirect, render_template, request, url_for
+from server import app
+import csv
+
+# Create a dictionary and initialize ?admin? as a key with
+# value = admin's chosen password
+users = {"admin":"password"}
+
+@app.route("/", methods=["GET", "POST"])
+
+def survey_homepage():
+	if request.method == "POST":
+		username = request.form["username"]
+		password = request.form["password"]
+		
+		if check_password(username, password):
+			return redirect(url_for('admin'))
+	return render_template("index.html")
+
+
+def check_password(user_name, password):
+	"""
+ 	:param user_name: The name of the user
+ 	:param password: Password provided by the user
+ 	"""
+	if user_name in users:
+		if password == users[user_name]:
+			return True
+
+@app.route("/admin")
+def admin():
+	return render_template("admin.html")
