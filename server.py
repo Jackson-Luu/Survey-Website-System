3c991cd700e87e46ababe74f08a1b28c619a4dc5
FML <robustprogram@gmail.com>
2017-09-06 15:36:49 +1000

    Making big changes

--- server.py
+++ server.py
@@ -3,24 +3,8 @@
     with python
 """
 from flask import Flask
-from server_system import SurveySystem
 from global_api import QuestionType
 from question import Question
 
 APP = Flask(__name__)
 APP.config["SECRET_KEY"] = "Highly secret key"
-
-# Before the application runs, we need to set up the Survey System
-G_SURVEY_SYSTEM = SurveySystem()
-
-# =====
-# DEBUG
-# =====
-# Load up a prebuilt survey to debug, delete when not needed
-G_SURVEY_SYSTEM.add_survey("debug1")
-q1 = Question("What am I doing?")
-q1.set_type(QuestionType.TEXT)
-G_SURVEY_SYSTEM.add_question("debug1", q1)
-q2 = Question("Why is life so hard")
-q2.set_type(QuestionType.TEXT)
-G_SURVEY_SYSTEM.add_question("debug1", q2)
