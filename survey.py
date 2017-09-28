982c78c1d52f64d58eddfad4ac14aa5c2ed2c226
JacksonLuu <jacksonluu2006@hotmail.com>
2017-09-28 14:34:05 +1000

    Update survey.py

--- survey.py
+++ survey.py
@@ -24,8 +24,6 @@ class Survey:
     def __init__(self, s_id, name):
         self.list_of_q = []
         self.answers = []
-        self.render_template = ""
-        self.modify_template = ""
         self.id = s_id
         self.name = name
 
@@ -61,99 +59,3 @@ class Survey:
     def modify_question(self, q_id, question):
         """ Modify the question stored in the survey """
         self.list_of_q[q_id] = question
-
-    def get_rendered_template(self):
-        """ Render the question to the user by forming the HTML in here
-        """
-
-        for question in self.list_of_q:
-            """ We are first going to load the queston text into the
-                template
-            """
-
-            input_area = "" # Initialise the variable to prevent errors
-
-            if question.get_type() == QuestionType.TEXT:
-                input_area = """
-                    <textarea class='q_textinput' name='q_user_input'>
-                        Please enter our input here ...
-                    </textarea>
-                """
-            elif question.get_type() == QuestionType.MULT:
-                """ If the question type is multiple choice, we will
-                    need to loop through the inputs specified by the
-                    admin and format it into the correct html
-                """
-
-                # This is used to separate the radio buttons into their groups
-                radio_counter = 0
-                for q_option in question.get_ans():
-                    input_area += """
-                        <div class='radio_group'>
-                            <div class='radio_btn'>
-                                <input type='radio' value='' name='q{counter}'/>
-                            </div>
-                            <div class='radio_text'>
-                                {text}
-                            </div>
-                        </div>
-                    """.format(text=q_option, counter=radio_counter)
-                    radio_counter += 1
-
-            template = """
-                <fieldset class='q_box'>
-                    <div class='q_text'>
-                        {q_text}
-                    </div>
-                    <div class='q_area'>
-                        {q_area}
-                    </div>
-                </fieldset>
-            """.format(q_text=question.get_text(), q_area=input_area)
-
-            self.render_template += template
-        return self.render_template
-
-    def get_modify_template(self):
-        """ Get the survey that is able to be modified by the admin
-        """
-
-        for question in self.list_of_q:
-            """ We are first going to load the queston text into the
-                template
-            """
-
-            input_area = ""
-
-            print(question.get_type())
-
-            if question.get_type() == QuestionType.MULT:
-                input_area = """
-                    <p class='sub_title'>Question Type:</p>
-                    <div>
-                        Multiple Choice<input type='radio' name='mult' checked='checked'/><br/>
-                        Text Based<input type='radio' name='text'/>
-                    </div>
-                """
-            elif question.get_type() == QuestionType.TEXT:
-                input_area = """
-                    <p>Question Type:</p>
-                    <div>
-                        Multiple Choice<input type='radio' name='mult'/><br/>
-                        Text Based<input type='radio' name='text'checked='checked'/>
-                    </div>
-                """
-
-            template = """
-                <fieldset class='q_box'>
-                    <div class='q_text'>
-                        Question: <input type='text' name='the_question' value='{q_text}'/>
-                    </div>
-                    <div class='q_area'>
-                        {q_area}
-                    </div>
-                </fieldset>
-            """.format(q_text=question.get_text(), q_area=input_area)
-
-            self.modify_template += template
-        return self.modify_template
