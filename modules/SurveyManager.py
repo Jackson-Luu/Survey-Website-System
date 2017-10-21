12dda701e3fa04df90b74334631c7b5a7d193969
linda09 <30562785+linda09@users.noreply.github.com>
2017-10-21 23:59:44 +1100

    jacko's update

--- modules/SurveyManager.py
+++ modules/SurveyManager.py
@@ -2,18 +2,25 @@ import csv
 from modules.head import *
 from modules.DataPacket import DataPacket
 
-def read_surveys(data_packet):
+def read_surveys(data_packet, admin_perms, courses):
     """ We are going to go through the list of DataPackets and translate it
         into something that can rendered into the templates
     """
     renderable = [] # List to hold the information that can used by Jinja2
     data_list = data_packet.retrieve_data()
+    course_list = []
+    if courses:
+        for course in courses:
+            course_list.append(*course)
 
     for data in data_list:
         # Go through each data and translate it into just 3 strings that can
         # be stored in a list
-
-        renderable.append([data[0], data[1], data[2], data[3]])
+        if not admin_perms:
+            if data[1] in course_list:
+                renderable.append([data[0], data[1], data[2], data[3]])
+        else:
+            renderable.append([data[0], data[1], data[2], data[3]])
 
     return renderable
 
